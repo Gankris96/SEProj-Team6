36fab54f4592a7c5eebec9545d4a2c26bbe7a84e
Gankris <gooner.gk@gmail.com>
2017-10-19 14:40:19 +0530

    Initial Commit:Firebase Authentication and Firebase DB integration for Student Users

---
+++ FirebaseAuth/app/src/main/java/info/androidhive/firebase/SelectActivity.java
@@ -0,0 +1,58 @@
+package info.androidhive.firebase;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class SelectActivity extends AppCompatActivity {
+
+    Button _selectstudent;
+    Button _selectdriver;
+    Button _selectparent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+        _selectstudent=(Button)findViewById(R.id.btn_student_login) ;
+        _selectdriver=(Button)findViewById(R.id.btn_driver_login) ;
+        _selectparent=(Button)findViewById(R.id.btn_parent_login) ;
+
+        _selectstudent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity for Student
+                Intent intent = new Intent(SelectActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        _selectdriver.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity for Driver
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        _selectparent.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // Start the Signup activity for Student
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
---
+++ FirebaseAuth/app/src/main/java/info/androidhive/firebase/StudentUser.java
@@ -0,0 +1,94 @@
+package info.androidhive.firebase;
+
+/**
+ * Created by Ganesh on 10/19/2017.
+ */
+
+public class StudentUser {
+    String studentUserId;
+    String studentName;
+    String studentEmail;
+    String studentPass;
+    String studentUSN;
+    String studentAddr;
+    String studentPhno;
+    String studentGender;
+
+    public StudentUser(){
+
+    }
+    public StudentUser(String studentUserId, String studentName, String studentEmail, String studentPass, String studentUSN, String studentAddr, String studentPhno, String studentGender) {
+        this.studentUserId = studentUserId;
+        this.studentName = studentName;
+        this.studentEmail = studentEmail;
+        this.studentPass = studentPass;
+        this.studentUSN = studentUSN;
+        this.studentAddr = studentAddr;
+        this.studentPhno = studentPhno;
+        this.studentGender = studentGender;
+    }
+
+    public String getStudentUserId() {
+        return studentUserId;
+    }
+
+    public void setStudentUserId(String studentUserId) {
+        this.studentUserId = studentUserId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getStudentEmail() {
+        return studentEmail;
+    }
+
+    public void setStudentEmail(String studentEmail) {
+        this.studentEmail = studentEmail;
+    }
+
+    public String getStudentPass() {
+        return studentPass;
+    }
+
+    public void setStudentPass(String studentPass) {
+        this.studentPass = studentPass;
+    }
+
+    public String getStudentUSN() {
+        return studentUSN;
+    }
+
+    public void setStudentUSN(String studentUSN) {
+        this.studentUSN = studentUSN;
+    }
+
+    public String getStudentAddr() {
+        return studentAddr;
+    }
+
+    public void setStudentAddr(String studentAddr) {
+        this.studentAddr = studentAddr;
+    }
+
+    public String getStudentPhno() {
+        return studentPhno;
+    }
+
+    public void setStudentPhno(String studentPhno) {
+        this.studentPhno = studentPhno;
+    }
+
+    public String getStudentGender() {
+        return studentGender;
+    }
+
+    public void setStudentGender(String studentGender) {
+        this.studentGender = studentGender;
+    }
+}
